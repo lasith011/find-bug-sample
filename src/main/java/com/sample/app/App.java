db5a1ccc414035d3708c6c37e0d8b0e0688c664c
lasith011 <lasith011@gmail.com>
2017-02-17 11:56:54 +0530

    Initializing

--- src/main/java/com/sample/app/App.java
+++ src/main/java/com/sample/app/App.java
@@ -1,10 +1,19 @@
 package com.sample.app;
 
+import java.util.Random;
+
 /**
  * Hello world!
  */
 public class App {
     public static void main(String[] args) {
+        String foo = null;
+        System.out.println(foo.length());
         System.out.println("Hello World!");
+    }
+
+    String generateSecretToken() {
+        Random r = new Random();
+        return Long.toHexString(r.nextLong());
     }
 }
